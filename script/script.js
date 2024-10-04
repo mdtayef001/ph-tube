a1f54a18d64f26d20671d06a9c830882d79220f3
tayef@gain <tayefhossain200@gmail.com>
2024-10-04 13:13:31 +0600

    videos

--- script/script.js
+++ script/script.js
@@ -1,9 +1,20 @@
+// fetching the data from api
+
 const loadCategoric = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => loadDisplay(data.categories))
     .catch((error) => console.log(error));
 };
+
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos))
+    .catch((error) => console.log(error));
+};
+
+// displaying the fetch data to UI
 
 const loadDisplay = (categories) => {
   const categoriesContainer = document.getElementById("categories");
@@ -14,4 +25,27 @@ const loadDisplay = (categories) => {
   });
 };
 
+const displayVideo = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    videoContainer.innerHTML += `
+    <div class="card card-compact ">
+        <figure>
+          <img
+            src=${video.thumbnail}
+            alt="thumbnail" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions ">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+    </div>  
+    `;
+  });
+};
+
 loadCategoric();
+loadVideos();
